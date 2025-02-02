a1d8999a2e7b5d91eaa18f4f87e4508b5ff5f2fe
bigVL <vlasq91@gmail.com>
2025-02-02 18:16:44 +0200

    Created connection to DB

---
+++ src/lib/db/index.ts
@@ -0,0 +1,23 @@
+import { Pool, QueryConfig, QueryResult } from 'pg';
+
+const pool = new Pool();
+
+export async function query<T extends Record<string, unknown>>(
+    text: string,
+    params: QueryConfig['values'] = [],
+): Promise<QueryResult<T>> {
+    const start = Date.now();
+    try {
+        const res = await pool.query<T>(text, params);
+        const duration = Date.now() - start;
+        console.log('Executed query', {
+            text,
+            duration,
+            rows: res.rowCount,
+        });
+        return res;
+    } catch (err) {
+        console.error('Error executing query', { text, err });
+        throw err;
+    }
+}
